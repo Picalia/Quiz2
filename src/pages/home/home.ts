86144bba57ee663aa422a915e7bc2b6f34d4757e
Andres Eduardo Ramirez <andresoni_2000@hotmail.com>
2018-10-16 08:08:46 -0500

    quiz terminado

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { BocinaPage } from '../bocina/bocina';
+import { TvPage } from '../tv/tv';
+import { SmartphonePage } from '../smartphone/smartphone';
+import { LamparaPage } from '../lampara/lampara';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +11,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
+  bocina = BocinaPage;
+  tv = TvPage;
+  smartphone = SmartphonePage;
+  lampara = LamparaPage;
+
   constructor(public navCtrl: NavController) {
 
   }
+
+  Clickbocina(){
+    this.navCtrl.push(this.bocina);
+
+  }
+
+  Clicktv(){
+    this.navCtrl.push(this.tv);
+
+  }
+
+  Clicksmartphone(){
+    this.navCtrl.push(this.smartphone);
+
+  }
+
+  Clicklampara(){
+    this.navCtrl.push(this.lampara);
+
+  }
+
 
 }
